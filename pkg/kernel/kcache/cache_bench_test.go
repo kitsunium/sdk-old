324c5855baf2dda351b4d9feccaf02067ca64db7
Florent <contact@making.codes>
2025-08-24 19:56:28 +0200

    style: fix formatting issues

--- pkg/kernel/kcache/cache_bench_test.go
+++ pkg/kernel/kcache/cache_bench_test.go
@@ -41,7 +41,7 @@ func BenchmarkLRU_Get(b *testing.B) {
 func BenchmarkLRU_SetWithTTL(b *testing.B) {
 	sizes := []int{100, 1000, 10000}
 	ttls := []time.Duration{time.Second, time.Minute, time.Hour}
-	
+
 	for _, size := range sizes {
 		for _, ttl := range ttls {
 			b.Run(fmt.Sprintf("size=%d/ttl=%s", size, ttl), func(b *testing.B) {
@@ -110,7 +110,7 @@ func BenchmarkLRU_Clear(b *testing.B) {
 func BenchmarkLRU_ConcurrentSet(b *testing.B) {
 	concurrencies := []int{1, 10, 100}
 	cacheSize := 1000
-	
+
 	for _, concurrency := range concurrencies {
 		b.Run(fmt.Sprintf("concurrency=%d", concurrency), func(b *testing.B) {
 			cache := NewLRU[string, int](cacheSize)
@@ -129,7 +129,7 @@ func BenchmarkLRU_ConcurrentSet(b *testing.B) {
 func BenchmarkLRU_ConcurrentGet(b *testing.B) {
 	concurrencies := []int{1, 10, 100}
 	cacheSize := 1000
-	
+
 	for _, concurrency := range concurrencies {
 		b.Run(fmt.Sprintf("concurrency=%d", concurrency), func(b *testing.B) {
 			cache := NewLRU[string, int](cacheSize)
@@ -156,7 +156,7 @@ func BenchmarkLRU_MixedOperations(b *testing.B) {
 			for i := 0; i < size/2; i++ {
 				cache.Set(strconv.Itoa(i), i)
 			}
-			
+
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				switch i % 4 {
@@ -221,7 +221,7 @@ func BenchmarkAtomicCache_Get(b *testing.B) {
 func BenchmarkShardedCache_Set(b *testing.B) {
 	shardCounts := []int{4, 16, 64}
 	cacheSize := 10000
-	
+
 	for _, shards := range shardCounts {
 		b.Run(fmt.Sprintf("shards=%d", shards), func(b *testing.B) {
 			cache := NewShardedLRU[string, int](cacheSize, shards)
@@ -236,7 +236,7 @@ func BenchmarkShardedCache_Set(b *testing.B) {
 func BenchmarkShardedCache_Get(b *testing.B) {
 	shardCounts := []int{4, 16, 64}
 	cacheSize := 10000
-	
+
 	for _, shards := range shardCounts {
 		b.Run(fmt.Sprintf("shards=%d", shards), func(b *testing.B) {
 			cache := NewShardedLRU[string, int](cacheSize, shards)
@@ -254,7 +254,7 @@ func BenchmarkShardedCache_Get(b *testing.B) {
 func BenchmarkShardedCache_ConcurrentSet(b *testing.B) {
 	shardCounts := []int{4, 16, 64}
 	cacheSize := 10000
-	
+
 	for _, shards := range shardCounts {
 		b.Run(fmt.Sprintf("shards=%d", shards), func(b *testing.B) {
 			cache := NewShardedLRU[string, int](cacheSize, shards)
@@ -273,7 +273,7 @@ func BenchmarkShardedCache_ConcurrentSet(b *testing.B) {
 func BenchmarkShardedCache_ConcurrentGet(b *testing.B) {
 	shardCounts := []int{4, 16, 64}
 	cacheSize := 10000
-	
+
 	for _, shards := range shardCounts {
 		b.Run(fmt.Sprintf("shards=%d", shards), func(b *testing.B) {
 			cache := NewShardedLRU[string, int](cacheSize, shards)
@@ -295,21 +295,21 @@ func BenchmarkShardedCache_ConcurrentGet(b *testing.B) {
 func BenchmarkComparisonConcurrentReadHeavy(b *testing.B) {
 	cacheSize := 1000
 	numKeys := 100
-	
+
 	keys := make([]string, numKeys)
 	for i := 0; i < numKeys; i++ {
 		keys[i] = strconv.Itoa(i)
 	}
-	
+
 	b.Run("LRU", func(b *testing.B) {
 		cache := NewLRU[string, int](cacheSize)
 		for i := 0; i < numKeys; i++ {
 			cache.Set(keys[i], i)
 		}
-		
+
 		var wg sync.WaitGroup
 		b.ResetTimer()
-		
+
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func(workerID int) {
@@ -322,16 +322,16 @@ func BenchmarkComparisonConcurrentReadHeavy(b *testing.B) {
 		}
 		wg.Wait()
 	})
-	
+
 	b.Run("AtomicCache", func(b *testing.B) {
 		cache := NewAtomicCache[string, int](cacheSize)
 		for i := 0; i < numKeys; i++ {
 			cache.Set(keys[i], i)
 		}
-		
+
 		var wg sync.WaitGroup
 		b.ResetTimer()
-		
+
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func(workerID int) {
@@ -344,16 +344,16 @@ func BenchmarkComparisonConcurrentReadHeavy(b *testing.B) {
 		}
 		wg.Wait()
 	})
-	
+
 	b.Run("ShardedCache", func(b *testing.B) {
 		cache := NewShardedLRU[string, int](cacheSize, 16)
 		for i := 0; i < numKeys; i++ {
 			cache.Set(keys[i], i)
 		}
-		
+
 		var wg sync.WaitGroup
 		b.ResetTimer()
-		
+
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func(workerID int) {
@@ -370,13 +370,13 @@ func BenchmarkComparisonConcurrentReadHeavy(b *testing.B) {
 
 func BenchmarkComparisonConcurrentWriteHeavy(b *testing.B) {
 	cacheSize := 1000
-	
+
 	b.Run("LRU", func(b *testing.B) {
 		cache := NewLRU[string, int](cacheSize)
-		
+
 		var wg sync.WaitGroup
 		b.ResetTimer()
-		
+
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func(workerID int) {
@@ -390,13 +390,13 @@ func BenchmarkComparisonConcurrentWriteHeavy(b *testing.B) {
 		}
 		wg.Wait()
 	})
-	
+
 	b.Run("AtomicCache", func(b *testing.B) {
 		cache := NewAtomicCache[string, int](cacheSize)
-		
+
 		var wg sync.WaitGroup
 		b.ResetTimer()
-		
+
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func(workerID int) {
@@ -410,13 +410,13 @@ func BenchmarkComparisonConcurrentWriteHeavy(b *testing.B) {
 		}
 		wg.Wait()
 	})
-	
+
 	b.Run("ShardedCache", func(b *testing.B) {
 		cache := NewShardedLRU[string, int](cacheSize, 16)
-		
+
 		var wg sync.WaitGroup
 		b.ResetTimer()
-		
+
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func(workerID int) {
